4a52870eb286d65afceba26203828856a91232fc
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-07-01 18:58:47 +0200

    Numbers Data Type #1

---
+++ NumberDataType.py
@@ -0,0 +1,15 @@
+import math
+
+num = -12
+print("Number: " + str(num))
+print("Rest of number when dividing by 3: " + str(num % 3))
+print("Absolute number: " + str(abs(num)))
+print("3 to the power of 2: " + str(pow(3, 2)))
+print("9 to the power of 5: " + str(pow(9, 5)))
+print("Maximum of several numbers: " + str(max(12, 91, -4, 19, 123)))
+print("Minimum of several numbers: " + str(min(-1, 39, 24, 19, 412)))
+print("Rounding of 8.5: " + str(round(8.5)))
+print("Rounding of 8.51: " + str(round(8.51)))
+print("Math floor of 4.6: " + str(math.floor(4.6)))
+print("Math ceil of 4.2: " + str(math.ceil(4.2)))
+print("Math square root of 36: " + str(math.sqrt(36)))
