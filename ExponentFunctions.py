9878cfe45c1ac450e40f71ca11f0bdf9959322f5
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-07-02 14:12:34 +0200

    Exponent Function #1

---
+++ ExponentFunctions.py
@@ -0,0 +1,9 @@
+
+def raise_to_power(base_num, power_num):
+    result = 1
+    for index in range(power_num):
+        result = result * base_num
+    return result
+
+
+print(raise_to_power(8, 3))
