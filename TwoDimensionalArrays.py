c13229c69f4e299d973c4c95d151f759bcd9d766
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-07-02 14:20:34 +0200

    Two Dimensional Arrays #1

---
+++ TwoDimensionalArrays.py
@@ -0,0 +1,13 @@
+number_grid = [
+    [3, 5, 12, 74],
+    [54, 7, 8, 61],
+    [91, 27, 5, 36],
+    [99]
+]
+
+for row in number_grid:
+    print(row)
+print("")
+for row in number_grid:
+    for col in row:
+        print(col)
