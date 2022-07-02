9489f9b2b41234a9311866cddf1e6e57aa852aa3
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-07-02 13:42:23 +0200

    For Loops #1

---
+++ ForLoops.py
@@ -0,0 +1,16 @@
+
+friends = ["Josh", "Marc", "Peter"]
+for friend in friends:
+    print(friend)
+print("")
+for index in range(4, 10):
+    print(index)
+print("")
+for index in range(len(friends)):
+    print(friends[index])
+print("")
+for index in range(5):
+    if index == 0:
+        print("first interaction")
+    else:
+        print("Not first")
