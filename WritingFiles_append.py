a83b9cb82a8dce32feba4b6c487b85d9cafcad5f
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-07-03 22:00:02 +0200

    Appending and Writing Files #1

---
+++ WritingFiles_append.py
@@ -0,0 +1,5 @@
+product_file = open("products.txt", "a")
+# r - reading, a - appending, w - (over-)writing
+product_file.write("\nLamp - 25")
+
+product_file.close()
---
+++ WritingFiles_write.py
@@ -0,0 +1,5 @@
+note_file = open("notes.txt", "w")
+
+note_file.write("\nBe careful when you execute an append file twice!")
+
+note_file.close()
