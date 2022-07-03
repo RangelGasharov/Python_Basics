4022fc50340aefb06c97ec0051f4b8bfd7812acc
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-07-03 22:17:20 +0200

    Class and Objects #1

---
+++ Student.py
@@ -0,0 +1,7 @@
+from ClassAndObjects import Student
+
+student1 = Student("Fred", "Architecture", 3.3, False)
+student2 = Student("Julia", "Business", 2.8, True)
+
+print(student1.name)
+print(student2.name)
