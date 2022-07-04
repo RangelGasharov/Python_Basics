5bb376ca29af20a40ef7236d55700b3740f5c9c7
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-07-04 21:04:34 +0200

    Drawing with Turtle #1

---
+++ DrawWithTurtle.py
@@ -0,0 +1,12 @@
+import turtle
+
+window = turtle.Screen()
+window.bgcolor("red")
+
+tree = turtle.Turtle()
+tree.color("forest green")
+
+tree.setposition(50, -50)
+tree.setposition(150, 0)
+tree.setposition(0, 0)
+turtle.done()
