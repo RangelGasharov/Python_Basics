468a9c1861689c8213fb6ad3a0fb8ef0bda0cd4f
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-11-10 18:35:48 +0100

    Roman numeral into number #2

--- basics/dictionaries/roman_into_number.py
+++ basics/dictionaries/roman_into_number.py
@@ -1,25 +1,17 @@
 s = input("Write down the roman number you want to translate.\n")
+numberInArray = []
 
 
 def roman_into_integer(s):
     roman_digits = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     value = 0
     for i in s:
-        if i in roman_digits:
-            value += roman_digits[i]
-
-    if "IV" in s:
-        value -= 2
-    if "IX" in s:
-        value -= 2
-    if "XL" in s:
-        value -= 20
-    if "XC" in s:
-        value -= 20
-    if "CD" in s:
-        value -= 200
-    if "CM" in s:
-        value -= 200
+        if i.upper() in roman_digits:
+            numberInArray.append(roman_digits[i.upper()])
+    for i in range(len(numberInArray)):
+        value += numberInArray[i]
+        if numberInArray[i] > numberInArray[i - 1] and i > 0:
+            value -= 2 * numberInArray[i - 1]
 
     return value
 
