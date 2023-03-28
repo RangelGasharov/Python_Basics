62794af0d99fb7a1a681501dff63be44840dd172
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-03-28 22:26:14 +0200

    Matplotlib Tangent on Function #2 - added examples of tangent on functions at several x coordinates

---
+++ matplotlib/arrays/matplotlib_tangent_on_function_2.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+step_of_calculation = 0.1
+start_x_axes = -10
+end_x_axes = 10
+
+x = np.arange(start_x_axes, end_x_axes + step_of_calculation, step_of_calculation)
+y = 3 * x ** 2
+for i in range(start_x_axes, end_x_axes + 1):
+    x_coordinates = i
+    y_first_derivative = 6 * x_coordinates * x - (3 * x_coordinates ** 2)
+    plt.plot(x, y_first_derivative, linewidth=1, color="red")
+
+plt.plot(x, y, linewidth=2)
+plt.grid(True)
+
+plt.show()
