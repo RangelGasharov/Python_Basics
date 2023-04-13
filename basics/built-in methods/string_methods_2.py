01eff7bafc584c59b4ae4435727d04790ba568f6
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-04-13 22:58:54 +0200

    String methods 2 #2 - added more examples

--- basics/built-in methods/string_methods_2.py
+++ basics/built-in methods/string_methods_2.py
@@ -1,3 +1,4 @@
+S = ""
 s = "hello"
 print("Capitalize:", s, s.capitalize())
 s = "a0BcdEF"
@@ -6,7 +7,7 @@ print("Center".center(20, "."))
 s = "aaaabbbbcccc"
 print("Count:", s, "aa:", s.count("aa"))
 s = "Text"
-print(s.encode(encoding="UTF-8", errors="strict"))
+print("Ecode", s.encode(encoding="UTF-8", errors="strict"))
 s = "orange"
 print("Ends with:", s, "ends with e:", s.endswith("e"))
 s = "text\ttext2\ttext3"
@@ -15,3 +16,38 @@ s = "abcdefg"
 print("Find", s, "e:", s.find("e"))
 s = "{one} and {two}"
 print("Format", s.format(one="first", two="second"))
+coordinates = {"x": 4, "y": 3}
+s = "Coordinates ({x}, {y})"
+print("Format Map", s.format_map(coordinates))
+s = "Text for testing"
+print("Index", s, "for:", s.index("for"))
+s = "test123"
+print("Isalnum", s, s.isalnum())
+s = "text"
+print("Isalpha", s, s.isalpha())
+s = "§"
+print("Isascci", s, s.isascii())
+s = "532"
+print("Isdecimal", s, s.isdecimal())
+s = "⓪"
+print("Isdigit", s, s.isdigit())
+s = "四五八"
+print("Isnumeric", s, s.isnumeric())
+s = "test-example"
+print("Isidentifier", s, s.isidentifier())
+s = "Word"
+print("Islower", s, s.islower())
+s = "Word\n"
+print("Isprintable", s, s.isprintable())
+s = " "
+print("Isspace", s, s.isspace())
+s = " "
+print("Isspace", s, s.isspace())
+s = "The Title Test"
+print("Istitle", s, s.istitle())
+s = "UPPER"
+print("Isupper", s, s.isupper())
+s = "-"
+print("Join", s.join(["first", "second", "third"]))
+s = "ljust"
+print("Ljust", s, s.ljust(20, "-"))
