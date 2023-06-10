a3b74782d31e2d4d1194368a36d24264ce7d5ca0
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-06-10 20:29:08 +0200

    Enumerate Lists #1 - added an example for enumerating a list

---
+++ basics/built-in methods/enumerate_lists.py
@@ -0,0 +1,4 @@
+cities = ["London", "Paris", "Istanbul", "Moscow", "Kinshasa", "Cairo", "Lagos", "Tokyo", "Delhi", "Shanghai", "Mumbai",
+          "Osaka", "beijing", "Sao Paulo", "Mexico City", "Dhaka", "New York", "Buenos Aires"]
+for i, city in enumerate(cities):
+    print(i, city)
