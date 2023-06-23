f35ddbebf8ca37493e73e537137c7928e5d7d9b7
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-06-23 23:26:22 +0200

    Array Methods #2 - added an example of the filter and list function

--- basics/arrays/array_methods.py
+++ basics/arrays/array_methods.py
@@ -4,3 +4,19 @@ for link in websites:
     print(link.removeprefix("www."))
 for link in websites:
     print(link.removesuffix(".com"))
+
+# filter with a function + list function
+
+numbers = range(1, 1000)
+
+
+def is_prime(num):
+    for x in range(2, num):
+        if (num % x) == 0:
+            return False
+    return True
+
+
+primes = list(filter(is_prime, numbers))
+
+print(primes)
