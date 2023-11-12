4239944115dbc1ac2d3f797bebc7438a5ec80a98
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-11-12 21:57:27 +0100

    Added dynamic starting point depending on given number of digits

--- algorithms/unique_variations.py
+++ algorithms/unique_variations.py
@@ -3,7 +3,10 @@ import time
 
 def get_non_repeating_digits_numbers(numbers_of_digits):
     test_list = []
-    for x in range(1234, 10 ** numbers_of_digits):
+    starting_point = 0
+    for i in range(1, numbers_of_digits + 1):
+        starting_point += i * 10 ** (numbers_of_digits - i)
+    for x in range(starting_point, 10 ** numbers_of_digits):
         test_list.append(x)
     return [sub for sub in test_list if len(set(str(sub))) == len(str(sub))]
 
@@ -12,4 +15,6 @@ start_time = time.time()
 result = get_non_repeating_digits_numbers(4)
 end_time = time.time()
 elapsed_time = end_time - start_time
-print(elapsed_time)
+print(result)
+print(f"{len(result)} variations")
+print(f"{elapsed_time} s")
