d737f88ce249af966721cbb37f8752e99b0dc88b
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-11-14 23:46:38 +0100

    Refactor - renamed variable

--- algorithms/unique_variations.py
+++ algorithms/unique_variations.py
@@ -1,12 +1,12 @@
 import time
 
 
-def get_non_repeating_digits_numbers(numbers_of_digits):
+def get_non_repeating_digits_numbers(number_of_digits):
     test_list = []
     starting_point = 0
-    for i in range(1, numbers_of_digits + 1):
-        starting_point += i * 10 ** (numbers_of_digits - i)
-    for x in range(starting_point, 10 ** numbers_of_digits):
+    for i in range(1, number_of_digits + 1):
+        starting_point += i * 10 ** (number_of_digits - i)
+    for x in range(starting_point, 10 ** number_of_digits):
         test_list.append(x)
     return [sub for sub in test_list if len(set(str(sub))) == len(str(sub))]
 
