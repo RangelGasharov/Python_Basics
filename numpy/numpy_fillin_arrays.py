7e70048a6e1b0ec37e603e4a30aa18c06aee2412
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-04-03 22:18:33 +0200

    Added an example about the different ways to fill a numpy array

---
+++ numpy/numpy_fillin_arrays.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+a = np.full((2, 3, 4), 9)
+print(a)
+
+b = np.zeros((2, 3, 4))
+print(b)
+
+c = np.ones((2, 3, 4))
+print(c)
+
+d = np.empty((5, 5, 5))
+print(d)
+
+x = np.arange(0, 1000, 5)
+print(x)
+
+y = np.linspace(0, 1000, 2)
+print(y)
