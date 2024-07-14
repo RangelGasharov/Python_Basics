28e886af88e553496ae62f9052145e7df65654a9
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-07-14 22:58:39 +0200

    Added function to calculate the time to sleep, finished algorithm

--- algorithms/edabit_bed_time.py
+++ algorithms/edabit_bed_time.py
@@ -1,6 +1,6 @@
-def bed_time(times_list):
+def bed_time(*args):
     times_to_sleep = []
-    for time_range in times_list:
+    for time_range in args:
         hours_end, minutes_end = get_hours_minutes(time_range[0])
         hours_to_sleep, minutes_to_sleep = get_hours_minutes(time_range[1])
         time_to_sleep = get_time_to_sleep(hours_end, minutes_end, hours_to_sleep, minutes_to_sleep)
@@ -14,8 +14,27 @@ def get_hours_minutes(time):
 
 
 def get_time_to_sleep(hours_end, minutes_end, hours_to_sleep, minutes_to_sleep):
-    return 0
+    minutes_start = minutes_end - minutes_to_sleep
+    hours_start = hours_end - hours_to_sleep
+    if minutes_start < 0:
+        minutes_start += 60
+        hours_start -= 1
+    if hours_start < 0:
+        hours_start += 24
+    return get_time_as_string(hours_start, minutes_start)
 
 
-print(get_hours_minutes("10:20"))
-print(bed_time([["07:50", "07:50"], ["08:00", "10:00"], ["09:00", "9:00"]]))
+def get_time_as_string(hours, minutes):
+    hours_as_string, minutes_as_string = "", ""
+    if hours < 10:
+        hours_as_string += "0"
+    hours_as_string += str(hours)
+    if minutes < 10:
+        minutes_as_string += "0"
+    minutes_as_string += str(minutes)
+    return hours_as_string + ":" + minutes_as_string
+
+
+print(bed_time(["07:50", "07:50"]))
+print(bed_time(["06:15", "10:00"], ["08:00", "10:00"], ["09:30", "10:00"]))
+print(bed_time(["05:45", "04:00"], ["07:10", "04:30"]))
