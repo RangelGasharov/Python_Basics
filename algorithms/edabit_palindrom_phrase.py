c6a7aa4064183fcd6d8202b5805ce98fd37c368a
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-07-24 20:41:53 +0200

    Finished the algorithm to find if a given sentence is a palindrome

--- algorithms/edabit_palindrom_phrase.py
+++ algorithms/edabit_palindrom_phrase.py
@@ -1,6 +1,15 @@
 def is_palindrome(sentence):
     separated_words = separate_words(sentence)
     sentence_string = build_sentence_string(separated_words)
+    result = is_palindrome_recursive(sentence_string)
+    return result
+
+
+def is_palindrome_recursive(text):
+    if len(text) == 0 or len(text) == 1:
+        return True
+    if text[0].lower() == text[len(text) - 1].lower():
+        return is_palindrome(text[1:len(text) - 1])
     return False
 
 
@@ -30,4 +39,7 @@ def build_sentence_string(words):
     return sentence
 
 
-print(build_sentence_string(separate_words("Eva, can I see bees in a cave?")))
+print(is_palindrome("Eva, can I see bees in a cave?"))
+print(is_palindrome("Go hang a salami, I'm a lasagna hog!"))
+print(is_palindrome("This phrase, surely, is not a palindrome!"))
+print(is_palindrome("Maneuquenam"))
