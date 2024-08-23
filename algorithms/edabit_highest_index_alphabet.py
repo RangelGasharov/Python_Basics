bc1c7130755b82eca9d8a8de766b9a1f1bb8063a
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-08-23 22:42:18 +0200

    Added a function that return the letter with the highest index in alphabetical order from a given word

--- algorithms/edabit_highest_index_alphabet.py
+++ algorithms/edabit_highest_index_alphabet.py
@@ -3,7 +3,21 @@ alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"
 
 
 def alphabet_index(alphabet, word):
-    return word
+    word_as_char_array = []
+    for letter in word:
+        word_as_char_array.append(letter.lower())
+    word_as_char_array.sort()
+    highest_letter = word_as_char_array[-1]
+    result = ""
+    for index, letter in enumerate(alphabet, start=0):
+        if letter == highest_letter.lower():
+            result = f"{index + 1}{highest_letter}"
+    return result
 
 
-print(alphabet_index(alphabet, "word"))
+print(alphabet_index(alphabet, "Xavier"))
+print(alphabet_index(alphabet, "Tesha"))
+print(alphabet_index(alphabet, "Flavio"))
+print(alphabet_index(alphabet, "Andrey"))
+print(alphabet_index(alphabet, "Oscar"))
+print(alphabet_index(alphabet, "Hello"))
