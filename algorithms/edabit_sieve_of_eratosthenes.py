46fbd28b7cccfdcd7aceddd0c0d3d2fa7408cdc8
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-08-30 22:53:42 +0200

    Added a function that returns all prime numbers up to a given number by using the sieve of eratosthenes

---
+++ algorithms/edabit_sieve_of_eratosthenes.py
@@ -0,0 +1,23 @@
+import math
+
+
+def eratosthenes(num):
+    result = []
+    prime_numbers = [True for i in range(num + 1)]
+    current_prime_number = 2
+    while current_prime_number ** 2 <= num:
+        if prime_numbers[current_prime_number]:
+            for i in range(current_prime_number ** 2, num + 1, current_prime_number):
+                prime_numbers[i] = False
+        current_prime_number += 1
+
+    for i in range(2, num+1):
+        if prime_numbers[i]:
+            result.append(i)
+    return result
+
+
+print(eratosthenes(0))
+print(eratosthenes(1))
+print(eratosthenes(30))
+print(eratosthenes(1000))
