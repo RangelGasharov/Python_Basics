c5119b10d24938651cb997a78cfc21353e9cd835
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-09-14 22:23:10 +0200

    Added a simple solution to the left shift problem

---
+++ algorithms/edabit_left_shift_iterative.py
@@ -0,0 +1,12 @@
+def shift_to_left(number, power):
+    return number * 2 ** power
+
+
+print(shift_to_left(5, 2))
+print(shift_to_left(10, 3))
+print(shift_to_left(-32, 2))
+print(shift_to_left(-6, 5))
+print(shift_to_left(12, 4))
+print(shift_to_left(46, 6))
+print(shift_to_left(57, 0))
+print(shift_to_left(79, 1))
