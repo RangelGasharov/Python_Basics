230753f4646f006060b246b4eafe6a683826f9a2
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-09-19 21:31:55 +0200

    Finihed the function that calculates the determinant of a square matrix

--- algorithms/edabit_determinant_square_matrix.py
+++ algorithms/edabit_determinant_square_matrix.py
@@ -3,32 +3,28 @@ def get_det(matrix):
     if size == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     result = 0
-    current_offset = 0
     for i in range(size):
-        current_add = 1
-        current_sub = 1
-        for j in range(size):
-            current_add *= matrix[(j + current_offset) % size][(j + current_offset) % size]
-        result += current_add
-        current_offset += 1
+        result -= get_diagonal_product(matrix, i, True)
+        result += get_diagonal_product(matrix, i, False)
     return result
 
 
-print(get_det([
-    [0, 1],
-    [1, 1]
-]))
-print(get_det([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]))
+def get_diagonal_product(matrix, offset, diagonal_reversed):
+    size = len(matrix)
+    product = 1
+    if diagonal_reversed:
+        for i in reversed(range(size)):
+            product *= matrix[size - 1 - i][(i + offset) % size]
+    else:
+        for i in range(size):
+            product *= matrix[i][(i + offset) % size]
+    return product
 
-print(get_det([
-    [69, 0],
-    [1, 1]
-]))
 
+print(get_det([[0, 1], [1, 0]]))
+print(get_det([[69, 0], [1, 1]]))
+print(get_det([[7, 420, 8], [5, 7, 0], [1, 1, 7]]))
+print(get_det([[5, 1], [1, 6]]))
+print(get_det([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+print(get_det([[-1, 1, -1], [-1, -55, 1], [1, -1, -1]]))
 print(get_det([[2, 7, 6], [9, 5, 1], [4, 3, 8]]))
-
-print(get_det([[7,420,8],[5,7,0],[1,1,7]]))
