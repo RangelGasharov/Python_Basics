54142b3ac4756119b84cfbfb910d26e58b25c5c9
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-09-20 23:35:32 +0200

    Added a function that handles the fiscal code for the name

--- algorithms/edabit_fiscal_code.py
+++ algorithms/edabit_fiscal_code.py
@@ -41,7 +41,37 @@ def get_surname_code(surname):
 
 
 def get_name_code(name):
-    return ""
+    word_consonants = []
+    word_vowels = []
+    for letter in name:
+        if letter.lower() not in vowels:
+            word_consonants.append(letter.upper())
+        else:
+            word_vowels.append(letter.upper())
+
+    if len(name) < 3:
+        result = ""
+        if len(word_consonants) > 0:
+            for letter in word_consonants:
+                result += letter
+        if len(word_vowels) > 0:
+            for letter in word_vowels:
+                result += letter
+        result += "X" * (3 - len(name))
+        return result
+
+    if len(word_consonants) == 3:
+        return "".join(word_consonants[:3])
+    elif len(word_consonants) > 3:
+        return word_consonants[0] + word_consonants[2] + word_consonants[3]
+    else:
+        result = ""
+        if len(word_consonants) > 0:
+            for letter in word_consonants:
+                result += letter
+        if len(word_vowels) > 0:
+            for i in range(0, 3 - len(word_consonants)):
+                result += word_vowels[i]
 
 
 def get_birth_gender_code(gender, date_of_birth):
@@ -49,3 +79,4 @@ def get_birth_gender_code(gender, date_of_birth):
 
 
 print(get_surname_code("Curie"))
+print(get_name_code("Brendan"))
