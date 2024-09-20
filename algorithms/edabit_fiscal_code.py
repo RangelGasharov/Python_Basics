eaee21dda4f0ebd258087f0e7e7cad1627b3ae89
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-09-20 23:22:09 +0200

    Begin working on a function that returns a fiscal code based on name, surname, gender and date of birth

---
+++ algorithms/edabit_fiscal_code.py
@@ -0,0 +1,51 @@
+vowels = {"a", "e", "i", "o", "u"}
+
+
+def fiscal_code(person_information):
+    person_surname = person_information["surname"]
+    person_name = person_information["name"]
+    person_gender = person_information["gender"]
+    person_date_of_birth = person_information["dob"]
+
+
+def get_surname_code(surname):
+    word_consonants = []
+    word_vowels = []
+    for letter in surname:
+        if letter.lower() not in vowels:
+            word_consonants.append(letter.upper())
+        else:
+            word_vowels.append(letter.upper())
+    if len(surname) < 3:
+        result = ""
+        if len(word_consonants) > 0:
+            for letter in word_consonants:
+                result += letter
+        if len(word_vowels) > 0:
+            for letter in word_vowels:
+                result += letter
+        result += "X" * (3 - len(surname))
+        return result
+
+    if len(word_consonants) >= 3:
+        return "".join(word_consonants[:3])
+    else:
+        result = ""
+        if len(word_consonants) > 0:
+            for letter in word_consonants:
+                result += letter
+        if len(word_vowels) > 0:
+            for i in range(0, 3 - len(word_consonants)):
+                result += word_vowels[i]
+        return result
+
+
+def get_name_code(name):
+    return ""
+
+
+def get_birth_gender_code(gender, date_of_birth):
+    return ""
+
+
+print(get_surname_code("Curie"))
