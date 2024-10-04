eb5454c423da4646516ab801eaaba8a0a1b25da6
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-10-04 22:50:57 +0200

    Refactoring - renamed file, added first functions for finding the zero point of a function with the help of secants

---
+++ linear equations/recursive_zero_point_secant_method.py
@@ -0,0 +1,13 @@
+def find_zero_point(function, x_down=0, x_upper=10):
+    return 0
+
+
+def secant_function(slope, intercept, x):
+    return slope * x + intercept
+
+
+def function_1(x):
+    return x ** 2 - 10
+
+
+find_zero_point(function_1)
