607069d96685784dc24dc62b8822913de701029d
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-11-01 00:20:25 +0100

    Optimized the function in performance by saving the results

--- algorithms/edabit_staircase_of_recursion.py
+++ algorithms/edabit_staircase_of_recursion.py
@@ -1,11 +1,17 @@
+results = [1, 1, 2]
+
+
 def ways_to_climb(num):
-    if num == 0:
-        return 1
-    if num == 1:
-        return 1
-    if num == 2:
-        return 2
-    return ways_to_climb(num - 1) + ways_to_climb(num - 2)
+    if num < len(results):
+        return results[num]
+    else:
+        results.append(ways_to_climb(num - 1) + ways_to_climb(num - 2))
+    return results[num]
 
 
 print(ways_to_climb(4))
+print(ways_to_climb(5))
+print(ways_to_climb(6))
+print(ways_to_climb(7))
+print(ways_to_climb(23))
+print(ways_to_climb(1000))
