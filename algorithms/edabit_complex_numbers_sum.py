545957a0a7411d34130ea3a077041f2233166ded
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-11-12 08:22:36 +0100

    Finished function for summing up complex numbers

--- algorithms/edabit_complex_numbers_sum.py
+++ algorithms/edabit_complex_numbers_sum.py
@@ -1,5 +1,17 @@
 def sum_complex(complex_numbers):
-    return 0
+    sum_real_part = 0
+    sum_complex_part = 0
+    for complex_number in complex_numbers:
+        separated_complex_number = split_up_complex_number(complex_number)
+        sum_real_part += separated_complex_number[0]
+        sum_complex_part += separated_complex_number[1]
+    result_string = ""
+    result_string += str(sum_real_part) * (sum_real_part != 0)
+    result_string += "+" * (sum_complex_part > 0 and sum_real_part != 0) + (str(sum_complex_part) + "i") * (
+                sum_complex_part not in [0, 1, -1])
+    result_string += ("-" * (sum_complex_part < 0) + str(sum_complex_part)[2:] + "i") * (sum_complex_part in [1, -1])
+    result_string += "0" * (sum_real_part == 0 and sum_complex_part == 0)
+    return result_string
 
 
 def split_up_complex_number(complex_number):
@@ -7,7 +19,6 @@ def split_up_complex_number(complex_number):
     complex_part = 0
     indexes_signs = []
     index_imaginary_unit = -1
-    end_real = -1
     for i in range(len(complex_number)):
         if complex_number[i] in ["+", "-"]:
             indexes_signs.append(i)
@@ -16,13 +27,20 @@ def split_up_complex_number(complex_number):
     if len(indexes_signs) > 1:
         end_real = indexes_signs[1]
         real_part = int(complex_number[0:end_real])
-        complex_part = int(complex_number[end_real + 1:-2])
+        complex_part = int(complex_number[end_real:-1] + "1" * (len(complex_number[end_real:-1]) == 1))
         return [real_part, complex_part]
+    elif len(indexes_signs) == 0:
+        if index_imaginary_unit > -1:
+            if index_imaginary_unit == 0:
+                return [0, 1]
+            return [0, int(complex_number[:-1])]
+        else:
+            return [int(complex_number), complex_part]
     else:
         if indexes_signs[0] > 0:
             end_real = indexes_signs[0]
             real_part = int(complex_number[0:end_real])
-            complex_part = int(complex_number[end_real + 1:-1])
+            complex_part = int(complex_number[end_real:-1] + "1" * (len(complex_number[end_real:-1]) == 1))
             return [real_part, complex_part]
         else:
             if index_imaginary_unit > -1:
@@ -32,9 +50,17 @@ def split_up_complex_number(complex_number):
                     return [0, int(complex_number[:-1])]
             else:
                 end_real = len(complex_number)
-    return [real_part, complex_part]
+                real_part = int(complex_number[0:end_real])
+                return [real_part, complex_part]
 
 
-print(split_up_complex_number("2+2i"))
-print(split_up_complex_number("+2i"))
-print(split_up_complex_number("-2"))
+print(sum_complex(["2+3i", "3-i"]))
+print(sum_complex(["1", "1"]))
+print(sum_complex(["0"]))
+print(sum_complex(["i", "2i", "3i"]))
+print(sum_complex(["-i"]))
+print(sum_complex(["-5", "5"]))
+print(sum_complex(["1", "10", "100", "1000"]))
+print(sum_complex(["3+4i", "3-4i"]))
+print(sum_complex(["2+i", "3+2i", "-5-2i"]))
+print(sum_complex(["-1000i", "1000i", "1234"]))
