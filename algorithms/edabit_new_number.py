0ffd7ef6a04588d4a62e596b3f2f3830018ade6b
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-11-26 23:05:18 +0100

    Finished function that determines if a number is lowest possible permutation of it digits and is thus a 'new number'

--- algorithms/edabit_new_number.py
+++ algorithms/edabit_new_number.py
@@ -1,28 +1,34 @@
 def is_new(number):
     number_string = str(number)
-    i, j = 1, 0
-    j = 0
-    while i < len(number_string):
-        if number_string[i] == "0":
-            i += 1
-            continue
-        if int(number_string[i]) < int(number_string[i - 1]):
+    if len(number_string) == 1:
+        return True
+    if len(number_string) == 2:
+        return (number_string[1] > number_string[0]) or (number_string[1] == "0")
+    i, j = 0, 1
+    while j < len(number_string):
+        if number_string[j - 1] != "0":
+            i = j - 1
+        if number_string[j] == "0":
+            if number_string[j - 1] != "0" and number_string[j + 1] != "0" and j - 1 != 0 and len(number_string) > 3:
+                return False
+        if int(number_string[j]) < int(number_string[i]) and number_string[j] != "0":
             return False
         j += 1
-        i += 1
     return True
 
 
-print(is_new(3))
-print(is_new(30))
+print("True values")
+print(is_new(0))
+print(is_new(90))
 print(is_new(102))
 print(is_new(123))
 print(is_new(40567))
 print(is_new(30004))
-print(is_new(40003))
-print(is_new(3000014))
+print(is_new(4444))
 print("False values")
 print(is_new(321))
+print(is_new(601))
+print(is_new(10783))
 print(is_new(23401))
 print(is_new(125609))
 print(is_new(40003))
