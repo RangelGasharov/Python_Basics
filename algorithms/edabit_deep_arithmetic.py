f13c74a88ba3e107ad5cf2244fe91b4d966048bf
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2025-02-25 22:59:45 +0100

    Finished algorithm for finding the sum of integers in strings of an array with arbitrary dimensions, added more examples

--- algorithms/edabit_deep_arithmetic.py
+++ algorithms/edabit_deep_arithmetic.py
@@ -13,7 +13,7 @@ def deep_sum_recursive(array, current_sum):
     numbers_sum = 0
     for i in range(len(array)):
         if isinstance(array[i], list):
-            numbers_sum = deep_sum_recursive(array[i], numbers_sum)
+            numbers_sum += deep_sum_recursive(array[i], numbers_sum)
         else:
             numbers_sum += get_number_sum_in_string(array[i])
     return numbers_sum
@@ -21,23 +21,27 @@ def deep_sum_recursive(array, current_sum):
 
 def get_number_sum_in_string(string):
     numbers_sum = 0
-    numbers_strings = []
     current_number = ""
-    for i in range(0, len(string)):
-        if not string[i].isnumeric():
-            if current_number != "":
-                numbers_strings.append(current_number)
-            current_number = ""
+
+    for i, char in enumerate(string):
+        if char.isdigit() or (char == "-" and current_number == ""):
+            current_number += char
         else:
-            current_number += string[i]
-    if current_number.isnumeric():
-        numbers_strings.append(current_number)
-    for i in range(len(numbers_strings)):
-        numbers_sum += int(numbers_strings[i])
+            if current_number and current_number not in ["-", "+"]:
+                numbers_sum += int(current_number)
+            current_number = "-" if char == "-" else ""
+
+    if current_number and current_number not in ["-", "+"]:
+        numbers_sum += int(current_number)
+
     return numbers_sum
 
 
-print(get_number_sum_in_string("1e3e2"))
 print(deep_sum(["1", "five", "2wenty", "thr33"]))
-print(deep_sum(["1", "five", ["2wenty", "thr33"]]))
-print(deep_sum(["1", "five", ["2wenty", ["thr33"]]]))
+print(deep_sum([["1X2", "t3n"], ["1024", "5", "64"]]))
+print(deep_sum([[["1"], "10v3"], ["738h"], [["s0"], ["1mu4ch3"], "-1s0"]]))
+print(deep_sum(
+    [[["0", "0x2", "z3r1"], ["1", "55a46"]], [["1", "0b2", "4"], ["0x5fp-2", "nine", "09"], ["4", "4", "4"]], [["03"]],
+     []]))
+print(deep_sum([[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]))
+print(deep_sum([[[[[["-32-64", "a-zA-Z"], ["01-1"]]]]]]))
